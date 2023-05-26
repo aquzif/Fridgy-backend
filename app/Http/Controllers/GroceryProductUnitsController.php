98bc02345052e9d8d89fe5cc72b76a2f61e655da
Mariusz Sternak <aquzif@gmail.com>
2023-05-26 13:04:18 +0200

    Add routes and controllers for models

---
+++ app/Http/Controllers/GroceryProductUnitsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GroceryProduct;
+use App\Models\GroceryProductUnit;
+use Illuminate\Http\Request;
+
+class GroceryProductUnitsController extends Controller {
+
+
+    public function index() {
+
+    }
+
+    public function store(Request $request) {
+
+    }
+
+    public function show(GroceryProductUnit $groceryProductUnit) {
+
+    }
+
+    public function update(Request $request, GroceryProductUnit $groceryProductUnit) {
+    }
+
+    public function destroy(GroceryProductUnit $groceryProductUnit) {
+    }
+}
