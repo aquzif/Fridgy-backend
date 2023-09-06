f31be4377b134d69c1d896fdc5faf374f16c620a
Mariusz Sternak <aquzif@gmail.com>
2023-09-06 07:47:21 +0200

    prepare test connection

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use App\Jobs\LoadRawDataJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use MongoDB\Driver\ServerApi;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,11 +16,51 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
 Artisan::command('load:raw', function () {
     $this->comment('Loading raw data...');
     dispatch(new LoadRawDataJob());
 })->purpose('Load raw data from the API');
+
+Artisan::command('load:off', function () {
+
+    $apiVersion = new ServerApi(ServerApi::V1);
+    $uri = env('MONGODB_CONNECTION','mongodb://localhost:27017');
+    $client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
+
+    try {
+        // Send a ping to confirm a successful connection
+        $collection = $client->off->produkty;
+        echo "Connected successfully to server";
+        $result = $collection->find([
+            'product_name_pl' => [
+                '$exists' => true,
+                '$ne' => ""
+            ]
+        ],[
+            'projection' => [
+                'product_name_pl' => 1,
+                'brands' => 1,
+                'quantity' => 1,
+                'nutriments' => [
+                    "sugars_100g" => 1,
+                    "energy_100g" => 1,
+                    "energy-kcal_100g"=> 1,
+                    "fat_100g" => 1,
+                    "proteins_100g" => 1,
+                    "salt_100g" => 1,
+                    "carbohydrates_100g" => 1
+                ]
+            ]
+        ]);
+        echo 'fetching data...';
+        //get result size
+        $resultSize = $result->toArray();
+
+        dd($resultSize);
+
+    } catch (Exception $e) {
+        printf($e->getMessage());
+    }
+
+
+});
